1f3607ec1e927cd41db37a8e7fcfc0df23f13ca3
swipss <vassiljevsten1@gmail.com>
2022-06-16 15:45:00 +0300

    remove cart item

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -8,7 +8,7 @@ import { urlFor } from '../lib/client'
 
 const Cart = () => {
   const cartRef = useRef()
-  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity } = useStateContext()
+  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext()
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
@@ -54,7 +54,7 @@ const Cart = () => {
                       </span>
                     </p>
                   </div>
-                  <button type='button' className='remove-item' onClick="">
+                  <button type='button' className='remove-item' onClick={() => onRemove(cartItem)}>
                     <TiDeleteOutline />
                   </button>
                 </div>
--- context/StateContext.js
+++ context/StateContext.js
@@ -37,6 +37,15 @@ export const StateContext = ({ children }) => {
     toast.success(`${quantity} ${product.name} added to the cart`);
   };
 
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id != product._id);
+
+    setTotalPrice(totalPrice - foundProduct.price * foundProduct.quantity);
+    setTotalQuantities(totalQuantities - foundProduct.quantity);
+    setCartItems(newCartItems);
+  };
+
   const toggleCartItemQuantity = (id, value) => {
     foundProduct = cartItems.find((item) => item._id === id);
     index = cartItems.findIndex((product) => product._id === id);
@@ -85,6 +94,7 @@ export const StateContext = ({ children }) => {
         onAdd,
         setShowCart,
         toggleCartItemQuantity,
+        onRemove,
       }}
     >
       {children}
